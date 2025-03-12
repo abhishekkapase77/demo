0468c72b31ab5321f8550367849ab90aeecf01af
abhishekkapase77 <abhishekkapase77@gmail.com>
2025-03-12 08:46:38 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,14 @@
+import streamlit as sl
+
+sl.date_input("Enter the date")
+
+sl.chat_input("enter the chat")
+
+sl.audio_input("Choose audio")
+
+sl.button("click me")
+
+sl.text_input("Enter the name")
+
+sl.time_input("")
+
